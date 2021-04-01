c69ea573e8f32bf0a73cc536c6d8e13108acb16f
kou <cheatbook_tarou@yahoo.co.jp>
2021-04-01 22:47:54 +0900

    一覧画面のテンプレート作成

--- src/main/java/com/chistudent/controller/StudentController.java
+++ src/main/java/com/chistudent/controller/StudentController.java
@@ -1,46 +1,42 @@
 package com.chistudent.controller;
 
-import java.util.List;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.chistudent.mapper.StudentMapper;
-import com.chistudent.model.Student;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("student")
+@RequestMapping("/student")
 public class StudentController {
 
     private final StudentMapper mapper;
 
-    @GetMapping
-    List<Student> showAllStudent() {
-        return mapper.getAllStudent();
+    @GetMapping("/list")
+    String showAllStudent(Model model) {
+        model.addAttribute("list", mapper.getAllStudent());
+        return "list";
     }
 
-    @GetMapping(path = "{id}")
-    Student showStudent(@PathVariable int id) {
-        return mapper.getStudent(id);
-    }
-
-    @PostMapping
-    String addStudent() {
-        mapper.insert("kanako", 20, 3, 4, "kanako@example.com");
-        return "追加しました";
-    }
-
-    @PutMapping(path = "{id}")
-    String updateByPkey(@PathVariable int id) {
-        mapper.updateByPkey(id);
-        return "更新に成功しました";
-    }
+//    @GetMapping(path = "{id}")
+//    Student showStudent(@PathVariable int id) {
+//        return mapper.getStudent(id);
+//    }
+//
+//    @PostMapping
+//    String addStudent() {
+//        mapper.insert("kanako", 20, 3, 4, "kanako@example.com");
+//        return "追加しました";
+//    }
+//
+//    @PutMapping(path = "{id}")
+//    String updateByPkey(@PathVariable int id) {
+//        mapper.updateByPkey(id);
+//        return "更新に成功しました";
+//    }
 
 }
